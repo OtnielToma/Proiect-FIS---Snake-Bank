35cc75b50a16883030746243f5b0cf5782675c57
OtnielToma <102229638+OtnielToma@users.noreply.github.com>
2022-04-12 00:37:42 +0300

    Adaugare resurse si personalizare interfata

--- src/main/java/org/loose/fis/registration/example/Main.java
+++ src/main/java/org/loose/fis/registration/example/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
         primaryStage.setTitle("Registration Example");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 }
