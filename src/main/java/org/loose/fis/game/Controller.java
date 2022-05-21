b5fe3a54dcba1e502037ce72c1aed883003b8769
OtnielToma <102229638+OtnielToma@users.noreply.github.com>
2022-05-21 13:17:56 +0300

    Adaugare feture de start game

--- src/main/java/org/loose/fis/game/Controller.java
+++ src/main/java/org/loose/fis/game/Controller.java
@@ -3,22 +3,101 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.util.Objects;
+import javafx.util.Duration;
 
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class Controller {
 
+    private final Double snakeSize = 50.;
+    private Rectangle snakeHead;
+    private Rectangle snakeTail_1;
+    double xPos;
+    double yPos;
+
+    Food food;
+    private Direction direction;
+    private final List<Position> positions = new ArrayList<>();
+    private final ArrayList<Rectangle> snakeBody = new ArrayList<>();
+
+    private int gameTicks;
+    @FXML
+    private ImageView keyboard;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Label instructions;
+    @FXML
+    private Label score;
+
+    private int Score=0;
 
 
+    Timeline timeline;
 
+    private boolean canChangeDirection;
 
+    @FXML
+    void start(MouseEvent event) {
+        startButton.setOpacity(0); //dupa apasarea butonului acesta dispare
+        Score=0;//scorul se reseteaza
+        String txt=new String();
+        txt="Score: "+Score;
+        score.setText(txt);
 
-    public void initialize() {
+        for (Rectangle snake : snakeBody) {
+            anchorPane.getChildren().remove(snake);
+        }
+
+        gameTicks = 0;
+        positions.clear();
+        snakeBody.clear();
+        snakeHead = new Rectangle(250, 250, snakeSize, snakeSize);
+        snakeTail_1 = new Rectangle(snakeHead.getX() - snakeSize, snakeHead.getY(), snakeSize, snakeSize);
+        xPos = snakeHead.getLayoutX();
+        yPos = snakeHead.getLayoutY();
+        direction = Direction.RIGHT;
+        canChangeDirection = true;
+        food.moveFood();
+
+        snakeBody.add(snakeHead);
+
+        Image img = new Image("/game/head.jpg"); //atribuire resursa
+        snakeHead.setFill(new ImagePattern(img));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+        snakeBody.add(snakeTail_1);
+
+        anchorPane.getChildren().addAll(snakeHead, snakeTail_1);
+
     }
-
 
 
 
@@ -27,8 +106,8 @@ public class Controller {
         Parent root1 = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("introduction/menuScene.fxml")));
         Stage stage1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene1 = new Scene(root1);
-            stage1.setScene(scene1);
-            stage1.show();
+        stage1.setScene(scene1);
+        stage1.show();
 
 
     }
