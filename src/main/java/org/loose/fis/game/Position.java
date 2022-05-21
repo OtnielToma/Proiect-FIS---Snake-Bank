5a7f29ea3471d8006adc80eee52ccddd001d81b1
OtnielToma <102229638+OtnielToma@users.noreply.github.com>
2022-05-21 12:42:39 +0300

    Adaugare pozitile axelor XoY

---
+++ src/main/java/org/loose/fis/game/Position.java
@@ -0,0 +1,14 @@
+package org.loose.fis.game;
+
+public class Position
+{
+    private double xPos;
+    private double yPos;
+
+    public Position(double xPos, double yPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+}
