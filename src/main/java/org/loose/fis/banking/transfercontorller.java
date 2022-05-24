0db311fc12b08430529e196ddf5b0b1cbfded1d5
SAVASHUB <103499835+SAVASHUB@users.noreply.github.com>
2022-05-24 12:18:55 +0300

    SB-34 Afisarea prietenirol in vbox

--- src/main/java/org/loose/fis/banking/transfercontorller.java
+++ src/main/java/org/loose/fis/banking/transfercontorller.java
@@ -1,4 +1,33 @@
 package org.loose.fis.banking;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
 public class transfercontorller {
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private TextField transferAmount;
+
+    public void initialize() throws CouldNotFindUserException {
+        ArrayList<String> friends = FriendsListservices.getFriends();
+
+        for (String friendName : friends) {
+            HBox friendContainer = new HBox();
+
+            Label friendUsernameLabel = new Label();
+            friendUsernameLabel.setText(friendName);
+
+            Button transferButton = new Button();
+            transferButton.setText("Transfer");
+        }
+
+    }
 }
